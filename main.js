77ccba8085dea32fd22e374fa23a9e01b8fdcc44
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-09-25 13:27:38 +0300

    even height

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const ctx = canvas.getContext('2d')
 const MIN_OPTIONS = 4
 const MAX_OPTIONS = 18
 
-const MIN_HEIGHT = 8
-const MAX_HEIGHT = 20
+const MIN_HEIGHT = 7
+const MAX_HEIGHT = 19
 const HEIGHT_STEP = 2
 
 let options = 7
 
-let height = 20
+let height = 19
 
 function getBoardLayout() {
   const centerX = canvas.width / 2
